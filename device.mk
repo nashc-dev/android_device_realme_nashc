5fe860d96071c551608c8944d0baa0c3d4f615c6
bengris32 <bengris32@protonmail.ch>
2023-06-02 19:23:51 +0100

    nashc: Build MediaTek HIDL GNSS service/impl
    Signed-off-by: bengris32 <bengris32@protonmail.ch>
    Change-Id: I0ee74b67cb14700f0e4c5a31b9bfc464207169e1

--- device.mk
+++ device.mk
@@ -133,16 +133,9 @@ PRODUCT_PACKAGES += \
 
 # GNSS
 PRODUCT_PACKAGES += \
-    android.hardware.gnss.measurement_corrections@1.0.vendor \
-    android.hardware.gnss.measurement_corrections@1.1.vendor \
-    android.hardware.gnss.visibility_control@1.0.vendor \
-    android.hardware.gnss@1.0.vendor \
-    android.hardware.gnss@1.1.vendor \
-    android.hardware.gnss@2.0.vendor \
-    android.hardware.gnss@2.1.vendor
-
-PRODUCT_PACKAGES += \
-    android.hardware.gnss-service.mediatek
+    android.hardware.gnss-service.mediatek \
+    android.hardware.gnss@2.1-service-mediatek \
+    android.hardware.gnss@2.1-impl-mediatek
 
 # Health
 PRODUCT_PACKAGES += \
